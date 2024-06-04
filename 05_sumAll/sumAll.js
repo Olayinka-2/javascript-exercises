018157579594fb0aaa78060ceddcfc157bb89c56
Olayinka-2 <olayinkaisrael802@gmail.com>
2024-06-04 15:13:48 +0100

    Add solution to the exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, secondNum) {
+   if(!Number.isInteger(firstNum) || !Number.isInteger(secondNum)) {
+      return "ERROR"
+   }
+   if(firstNum < 0 || secondNum < 0) {
+      return "ERROR";
+   }
+   if(secondNum < firstNum) {
+      [firstNum, secondNum] = [secondNum, firstNum];
+   }
+   let initialNum = 0;
+   for(let i = firstNum; i <= secondNum; i++){
+      initialNum += i;
+   }
+   return initialNum;
 };
+
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
+
